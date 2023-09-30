07532951019758445fba04962ed6b400ca3d47f8
StevenHdez11 <stevenhdez.11@hotmail.com>
2023-09-30 17:16:53 -0600

    first commit

---
+++ lib/screens/basic_design.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BasicDesignScreen extends StatelessWidget {
+  const BasicDesignScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hola Mundo'),
+      ),
+    );
+  }
+}
